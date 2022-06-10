d07ce248e6f1c39789ee439adf3e9082dde5f002
Darryl Noumen <noumendarryl@gmail.com>
2022-06-10 10:30:12 +0100

    My first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//MENU TOOGLER
+const hamburger = document.querySelector(".hamburger");
+const close = document.querySelector(".close");
+const navMenu = document.querySelector(".navigation-menu");
+
+hamburger.addEventListener("click", () => {
+  navMenu.classList.toggle("show");
+  hamburger.classList.toggle("hide");
+});
+    
+close.addEventListener("click", () => {
+  navMenu.classList.toggle("show");
+  hamburger.classList.toggle("hide");
+});

1dd19ddf060ae5c5874594f9286a7445116ba204
Darryl Noumen <noumendarryl@gmail.com>
2022-07-08 12:03:53 +0100

    script.js reviewed

--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@ const hamburger = document.querySelector(".hamburger");
 const close = document.querySelector(".close");
 const navMenu = document.querySelector(".navigation-menu");
 
-hamburger.addEventListener("click", () => {
+hamburger.addEventListener("click", ()=>{
   navMenu.classList.toggle("show");
   hamburger.classList.toggle("hide");
 });
 
-close.addEventListener("click", () => {
+close.addEventListener("click", ()=>{
   navMenu.classList.toggle("show");
   hamburger.classList.toggle("hide");
 });

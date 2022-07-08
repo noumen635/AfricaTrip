d8b2c37e54afbdbe9872d45ecea9a9772ba5c0aa
Darryl Noumen <noumendarryl@gmail.com>
2022-07-08 12:10:06 +0100

    script.js and responsive.css reviewed

--- js/script.js
+++ js/script.js
@@ -3,12 +3,16 @@ const hamburger = document.querySelector(".hamburger");
 const close = document.querySelector(".close");
 const navMenu = document.querySelector(".navigation-menu");
 
-hamburger.addEventListener("click", ()=>{
+hamburger.addEventListener("click", () => {
   navMenu.classList.toggle("show");
   hamburger.classList.toggle("hide");
+  navMenu.style.right = "0";
+  navMenu.style.width = "16rem";
 });
 
-close.addEventListener("click", ()=>{
+close.addEventListener("click", () => {
   navMenu.classList.toggle("show");
   hamburger.classList.toggle("hide");
+  navMenu.style.right = "-16rem";
+  navMenu.style.width = "0";
 });

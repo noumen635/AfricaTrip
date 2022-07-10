65d1e949a76f7c81350ba4cc00fa2bc831056632
Darryl Noumen <noumendarryl@gmail.com>
2022-07-10 04:08:14 +0100

    index.html, index.js and index.test.js files reviewed

--- js/index.js
+++ js/index.js
@@ -2,22 +2,21 @@
 const hamburger = document.querySelector(".hamburger");
 const close = document.querySelector(".close");
 const navMenu = document.querySelector(".navigation-menu");
+const show = document.querySelector(".show");
+const hide = document.querySelector(".hide");
 
-function openMenuToogle () {
+function openMenuToogle() {
   navMenu.classList.toggle("show");
   hamburger.classList.toggle("hide");
   navMenu.style.right = "0";
   navMenu.style.width = "16rem";
 }
 
-function closeMenuToogle () {
+function closeMenuToogle() {
   navMenu.classList.toggle("show");
   hamburger.classList.toggle("hide");
   navMenu.style.right = "-16rem";
   navMenu.style.width = "0";
 }
 
-// hamburger.addEventListener("click", openMenuToogle);
-// close.addEventListener("click", closeMenuToogle);
-
-module.exports = openMenuToogle, closeMenuToogle, navMenu, hamburger, close;
+module.exports = openMenuToogle, closeMenuToogle, navMenu, hamburger, close, show, hide;
